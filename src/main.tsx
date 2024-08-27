ea7570cb8fbfc807a6b8a5028f4cf8771048c6f3
praveen-pathak <praveenpathak235@gmail.com>
2024-08-27 23:24:37 +0530

    Completed-Project

---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// );
+import { invoke } from "@tauri-apps/api/tauri";
+
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
